ca2bec64b0772bf7eb00b9261669595d0bb840b4
amrabat30 <amrabat30@gmail.com>
2014-09-13 18:32:14 +0300

    Panel Index Started.

--- projects/reklampi/js/main.js
+++ projects/reklampi/js/main.js
@@ -234,16 +234,20 @@ var PR = {
 
             init: function(){
 
-                function initialize()  {
-                    var mapProp = {
-                        center:new google.maps.LatLng(41.08742,29.120850),
-                        zoom:10,
-                        mapTypeId:google.maps.MapTypeId.ROADMAP
-                    };
-                    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-                }
+                if ($('#googleMap').length){
 
-                google.maps.event.addDomListener(window, 'load', initialize);
+                    function initialize()  {
+                        var mapProp = {
+                            center:new google.maps.LatLng(41.08742,29.120850),
+                            zoom:10,
+                            mapTypeId:google.maps.MapTypeId.ROADMAP
+                        };
+                        var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+                    }
+
+                    google.maps.event.addDomListener(window, 'load', initialize);
+
+                }
 
             }
 
