8c4c04d862274e892814e1ac89164c14fbdb7c59
amrabat30 <amrabat30@gmail.com>
2014-09-13 22:30:38 +0300

    tab js

--- projects/reklampi/js/main.panel.js
+++ projects/reklampi/js/main.panel.js
@@ -3,6 +3,7 @@ var PNL = {
     init: function() {
 
         PNL.General.init();
+        PNL.Tab.init();
 
     },
 
@@ -30,6 +31,32 @@ var PNL = {
                 });
             }
 
+        }
+
+    },
+
+    Tab: {
+
+        init: function(){
+            $('#tabMenu .tab-menu-item').on('click',PNL.Tab.open);
+        },
+
+        open: function(){
+
+            var th = $(this);
+            var index = th.attr('data-tab-index');
+
+            th.addClass('active');
+            th.fadeIn();
+            th.parent().siblings().children().removeClass('active');
+
+            var tabContent = $('#tabContent');
+            tabContent.children('.tab-content-item').removeClass('active');
+            tabContent.children('.tab-content-item').fadeOut();
+            tabContent.children('.tab-content-item[data-tab-index="'+index+'"]').addClass('active');
+            tabContent.children('.tab-content-item[data-tab-index="'+index+'"]').fadeIn();
+
+            return false;
         }
 
     }
